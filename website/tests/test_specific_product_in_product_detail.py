0bb3cd20606908bcf560a0f7d31014c70b4f9188
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-05-25 15:15:14 -0500

    Yes

--- website/tests/test_specific_product_in_product_detail.py
+++ website/tests/test_specific_product_in_product_detail.py
@@ -32,6 +32,17 @@ class SpecificCategoryTestCases(TestCase):
         response = self.client.get(reverse('website:product_details', args={self.product.pk}))        
         self.assertEqual(self.product, response.context['product'])        
 
+    def test_product_attributes_show_in_product_details_view_body(self):
+        """
+        Verify that when a product is created that the Product Detail view has the title, description, price and quantity are in the response body
+        test_specific_product_in_product_detail.py
+        """
+        response = self.client.get(reverse('website:product_details', args={self.product.pk}))
+        self.assertContains(response, "Computer")
+        self.assertContains(response, "Something electronic")
+        self.assertContains(response, 350.00)
+        self.assertContains(response, 2)
+
 
 
 
