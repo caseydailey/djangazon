115dbc0cb80240f3a0e4629f332cfb2108df3b49
Casey Dailey <casey@feasttogether.org>
2017-05-30 16:22:41 -0500

    watch out google, our keyword search is off the fuckin chain

--- website/views/list_products.py
+++ website/views/list_products.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from website.models import Product
 
 def list_products(request):
   if request.method == 'GET':
-    template_name = 'product/list.html'
-    return render(request, template_name)
+    if 'search_box' in request.GET:
+        search_query = request.GET.get('search_box')
+        print("search_query: {}".format(search_query))
+        title_contains = Product.objects.filter(title__contains=search_query)
+        print("title_contains: {}".format(title_contains))
+        description_contains = Product.objects.filter(description__contains=search_query)
+        print("description_contains: {}".format(description_contains))
+        template_name = 'product/list.html'
+        return render(request, template_name, {"title_contains": title_contains, "description_contains": description_contains})
+    else:
+        template_name = 'product/list.html'
+        return render(request, template_name)
