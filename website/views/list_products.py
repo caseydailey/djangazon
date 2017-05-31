337d9fa1f5bed683441a9454299eb420c456c149
Casey Dailey <casey@feasttogether.org>
2017-05-31 11:17:09 -0500

    fixed the duplication bug in search

--- website/views/list_products.py
+++ website/views/list_products.py
@@ -15,28 +15,24 @@ def list_products(request):
     if request.method == 'GET':
         # get value of search_box
         search_query = request.GET.get('search_box')
-
+        print("search: {}".format(search_query))
         # if they're searching and search_query is truthy (meaning it's not blank), 
         # filter products where title, description, or city contains search_query
         if 'search_box' in request.GET and search_query:
-            title_contains = Product.objects.filter(title__contains=search_query)
-            description_contains = Product.objects.filter(description__contains=search_query)
-            city_contains = Product.objects.filter(city__contains=search_query)
+            results = set()
+            results.update(Product.objects.filter(title__contains=search_query))
+            results.update(Product.objects.filter(description__contains=search_query))
+            results.update(Product.objects.filter(city__contains=search_query))
 
             # if any return match, display
-            if title_contains or description_contains or city_contains:
-                template_name = 'product/list.html'
-                return render(request, template_name, {
-                    "title_contains": title_contains, 
-                    "description_contains": description_contains,
-                    "city_contains": city_contains})
+            if results:
+                return render(request, 'product/list.html', {"results": results})
 
             # no results 
             else:
-                template_name = 'product/no_products.html'
-                return render(request, template_name)
+                return render(request, 'product/no_products.html')
 
-    # they searched for nothing    
-    else:
-        template_name = 'product/no_products.html'
-        return render(request, template_name)
+        # they searched for nothing    
+        else:
+            return render(request, 'product/no_products.html')
+            
