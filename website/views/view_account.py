5b3efa641aaf101d2c0988a20228df60f7d70b6d
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-06-01 08:55:42 -0500

    user-view-account-prepopulating

--- website/views/view_account.py
+++ website/views/view_account.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from website.forms import UserForm, ProfileForm
 
 def view_account(request):
-  if request.method == 'GET':
-    template_name = 'account/view_account.html'
-    return render(request, template_name)
+
+    if request.method == 'GET':
+
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    
+        return render(request, "account/view_account.html", {
+            "user_form": user_form,
+            "profile_form": profile_form,
+            })
+
+    elif request.method == 'POST':   
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()    
+            profile_form = profile_form.save(commit=False)
+            profile_form.user = request.user
+            profile_form.save()
+
+            return HttpResponseRedirect("view_account")
+
+        else:
+
+            return render (request, "account/view_account.html", {
+                "user_form": user_form,
+                "profile_form": profile_form,
+                })
+            
